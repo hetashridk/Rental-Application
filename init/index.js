723f5fdf004f97a08ece19c8302e569a753ba2bc
hetashridk <hetashrik@gmail.com>
2024-03-28 19:06:35 +0530

    Database intialized

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/rentalSystem")
+}
+
+main()
+    .then(() => {
+        console.log("DB connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was intialized");
+};
+
+initDB();
+
+ 
